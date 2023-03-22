c77c78b7167fad6cffb721ad2ea0fe24ddc2dadf
unknown <pierrethibout2@gmail.com>
2023-03-22 19:34:14 +0100

    Change to the class paradigm

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 let roll_dice = document.querySelector('#rolldice');
 let hold_game = document.querySelector('#holdgame');
 let new_game = document.querySelector('#newgame');
@@ -7,126 +6,216 @@ let currentP1 = document.querySelector(".currentP1");
 let currentP2 = document.querySelector(".currentP2");
 let totalP1 = document.querySelector(".scoreP1");
 let totalP2 = document.querySelector(".scoreP2");
-
-let newgame = false;
-let state = false;
-let total_score_p1 = 0;
-let total_score_p2 = 0;
-let round_score = 0;
-let actual_player = 1;
+let message = document.querySelector(".message");
+let timerElement = document.querySelector("timer");
 document.body.onload = Oninit;
-//body.addEventListener("load", Oninit);
 
-function Game(){
-    
-    function RollDice(){
-        let nb_dices = document.getElementsByClassName("newdices").length;
-        
-        console.log("test");
-        
-        for (let nb = 0; nb < nb_dices; nb++){
-        
-        let rolldice = Math.floor(Math.random()*6) +1;
-        
-        const elements = document.querySelector("img#des" + nb).setAttribute("src", "./images/dice" + rolldice + ".png");
-        
-        
-            if (rolldice === 1){          
-                
-                round_score = 0;
-                Hold();    
-                
-    
-            }else{
-                round_score += rolldice;
+let RollDice;
+let P1;
+let P2;
+
+class Game{
+    activePlayer = 1;
+    newgame = false;
+    //limitPlayers = 2;
+    constructor(){
+       
+        //this.limitPlayers = limitPlayers;
+    }
+    /*StartTimer(){
+        // program to create a countdown timer
+
+        // time to countdown from (in milliseconds)
+        let countDownDate = new Date().getTime() + 30 * 1000;
+
+        // countdown timer
+        let x = setInterval(function() {
+
+            // get today's date and time in milliseconds
+            let now = new Date().getTime();
+
+            // find the interval between now and the countdown time
+            let timeLeft = countDownDate - now;
+
+            // time calculations for days, hours, minutes and seconds
+            //const days = Math.floor( timeLeft/(1000*60*60*24) );
+            //const hours = Math.floor( (timeLeft/(1000*60*60)) % 24 );
+            //const minutes = Math.floor( (timeLeft/1000/60) % 60 );
+            const seconds = Math.floor( (timeLeft/1000) % 60 );
+
+            // display the result in the element with id="demo"
+            //return seconds;
+            //timerElement.textContent = seconds + " secondes ";
+
+            // clearing countdown when complete
+            if (timeLeft < 0) {
+                clearInterval(x);
+                console.log('CountDown Finished');
             }
+            }, 1000);
+        
+    }*/
+    Win(){
+
+    }
+    Reset(){
+        this.activePlayer = 1;
+        this.newgame = true;
+    }
+    ChangePlayer(){
+        if(this.activePlayer === 1){
+            this.activePlayer = 2
+        }else{
+            this.activePlayer = 1;
         }
+    }
+}
+
+class Player{
+   totalScore = 0;
+   roundScore = 0;     
+    constructor(name){
+        this.name = name;
+        /*this.totalScore = totalScore;
+        this.roundScore = roundScore;*/
+    }
+    GetRound(){
+        
+        return this.roundScore;
+    }
+    SetRound(score){
+        this.roundScore = score;
+    }
+    GetTotal(){
+        
+        return this.totalScore;
+    }
+    SetTotal(totalScore){
+        this.totalScore = totalScore;
+    }
+    Hold(){
+        this.totalScore += this.roundScore;
+    }
+    ShowName(){
+
+    }
+}
+
+class Player1 extends Player{
+    constructor(name){
+        super(name,0,0);
+    }
+    RollTheDice(){
+        let nb_dices = document.getElementsByClassName("newdices").length;
+            
+        console.log("test");
+            
+            for (let nb = 0; nb < nb_dices; nb++){
+            
+            let rolldice = Math.floor(Math.random()*6) +1;
+            
+            const elements = document.querySelector("img#des" + nb).setAttribute("src", "./images/dice" + rolldice + ".png");
+            
+            
+                if (rolldice === 1){          
+                    
+                    
+                    RollDice.Hold();   
+                    
+        
+                }else{
+                    round_score += rolldice;
+                }
+            }
         return round_score;
     }
-    
-    function PlayAgain(){
-        total_score_p1 = 0;
-        total_score_p2 = 0;
-        round_score = 0;
-        actual_player = 1;
-    }
-    
-    function Hold(){
-        /*if(total_score_p1 < 100 || total_score_p2 < 100){
+    /*destructor {
+        // This function is called when the object is destroyed
+       console.log(`${this.name} is destroyed`);
+     }*/
 
-        }*/
-        if(actual_player === 1){
-            //console.log("p1");
-            total_score_p1 += round_score;
-            round_score = 0;
-            currentP1.textContent = round_score+"";
-            totalP1.textContent = total_score_p1+"";
-            console.log("deb " + total_score_p1 + "round " + round_score);
-        }else{
-            //console.log("p2");
-            total_score_p2 += round_score;
-            round_score = 0;
-            currentP2.textContent = round_score+"";
-            totalP2.textContent = total_score_p2+"";
-        }
-        if(state === false){
-            state = true;
-            console.log(state);
-        }else{
-            state = false;
-        }
-        switch(state){
-            case true:
-                actual_player = 2;            
-                break;
-            case false:
-                actual_player = 1;            
-                break;
+}
+
+class Player2 extends Player{
+    constructor(name){
+        super(name,0,0);
+    }
+    RollTheDice(){
+        let nb_dices = document.getElementsByClassName("newdices").length;
             
-        }
+        console.log("test");
+            
+            for (let nb = 0; nb < nb_dices; nb++){
+            
+            let rolldice = Math.floor(Math.random()*6) +1;
+            
+            const elements = document.querySelector("img#des" + nb).setAttribute("src", "./images/dice" + rolldice + ".png");
+            
+            
+                if (rolldice === 1){          
+                    
+                    
+                    RollDice.Hold();   
+                    
         
-        //actual_player.value(actual_player.value() == 1 ? 2 : 1 );
-        console.log(actual_player);
+                }else{
+                    round_score += rolldice;
+                }
+            }
+        return round_score;
     }
+}
+new_game.addEventListener('click', (e) => {
+    e.preventDefault();
+    Oninit();
+  });
+
+roll_dice.addEventListener('click', (e) => {
+    e.preventDefault();
     
-    roll_dice.addEventListener('click', (e) => {
-        e.preventDefault();
-        console.log("test button");
-        round_score = RollDice();
-        //console.log(round_score);
-        //console.log(total_score_p1);
-        //console.log(total_score_p2);
-        console.log(total_score_p1 + " p2 " + total_score_p2);
-        if(actual_player === 1){
-            currentP1.textContent = round_score +"";
-        }else{
-            currentP2.textContent = round_score+"";
-        }
-      });
-    
-    hold_game.addEventListener('click', (e) => {
-        e.preventDefault();
-        Hold();
-        console.log(round_score);
-        console.log(total_score_p1 + " p2 " + total_score_p2);
-      });
-    
-    new_game.addEventListener('click', (e) => {
-        e.preventDefault();
-        Oninit();
-      });
+    P1 = new Player("P1");
+    P2 = new Player("P2");
+
+    console.log(P1.RollTheDice()+"");
+
+    //console.log(RollDice.StartTimer());
+    //timerElement.textContent = RollDice.StartTimer();
+    //RollDice.StartTimer(); 
+    P1.SetRound(2);
+    console.log(P1.GetRound() + " " + P1.GetTotal());
+    P1.Hold();
+    P1.SetRound(5);
+    console.log(P1.GetRound() + " " + P1.GetTotal());
+  });
+hold_game.addEventListener('click', (e) => {
+    e.preventDefault();
+    RollDice.Hold();
+  });
+
+function Oninit(){      
+    message.textContent = "Player 1 Starts";
+    P1 = null;
+    P2 = null;    
+    RollDice.Reset();   
     
 }
+RollDice = new Game();
+
+//RollDice.Start(P1,P2);
+//RollDice = new Game();
+//P1 = new Player("P1");
+//P2 = new Player("P2");
+
+//console.log(RollDice.StartTimer());
+//timerElement.textContent = RollDice.StartTimer();
+//RollDice.StartTimer(); 
+//P1.SetRound(2);
+
+//P1.Hold();
+//P1.SetRound(5);
+//console.log(P1.GetRound() + " " + P1.GetTotal());
+//console.log(RollDice.GetRound());
+//P1.ChangePlayer();
+//RollDice.clear();
 
 
-function Oninit(){
-    total_score_p1 = 0;
-    total_score_p2 = 0;
-    //state = false;
-    round_score = 0;
-    actual_player = 1;
-    //newgame = false;
-    let message = document.querySelector(".message");
-    message.textContent = "Player 1 Starts";    
-    Game();
-}
